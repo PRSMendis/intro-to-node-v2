b3b459d54d95f7bfac956c3cdaffa0d2929c3d05
PRSMendis <rayhan.mendis@gmail.com>
2022-07-12 17:21:12 +1000

    fs stuff

--- index.mjs
+++ index.mjs
@@ -1,3 +1,15 @@
-export const action = () => {
-    console.log('hello$')
+  import {readFile, writeFile} from 'fs/promises';
+
+let template = await readFile(new URL('template.html', import.meta.url), 'utf-8')
+console.log('template: ', template);
+
+const data = {
+    title: 'learn node.js',
+    body: 'this is the final html'
 }
+
+for (const [k,v] of Object.entries(data)) {
+    template = template.replace(`{${k}}`, v )
+}
+console.log('template: ', template);
+await writeFile(new URL('index.html', import.meta.url), template)
